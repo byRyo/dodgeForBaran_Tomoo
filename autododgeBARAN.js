4fe0daabfb648b7f94a7b5dc5f4efd10c0282413
Ryo <rrblx511@gmail.com>
2025-06-06 20:07:27 +0200

    Create autododgeBARAN.js

---
+++ autododgeBARAN.js
@@ -0,0 +1,45 @@
+// BARAN AUTODODGE
+let baran = {
+    iq: "0",
+    age: 2,
+    nigger: function () {
+        console.log("hello i am baran and im a big nigger !");
+        console.log("bro what you think im gay obv !");
+    },
+    nigga: function (BARAN) {
+        return BARAN * BARAN;
+    },
+    BARANOFFSETS: function () {
+        console.log("here are all hack I made since I am a pro")
+        console.log("0xABCDEF") // autododge offset
+        console.log("0xABC...") // aimbot offset
+        console.log("0xABCD..") // crasher offset
+        console.log("0x123456") // instant win game offset
+    },
+    BARANHACKCOMPUTER: function () { // here is my best function to hack people made by BARAN (:
+        console.log("starting hack...")
+        console.log("1, 2, 3")
+        console.log("fully hacked your pc")
+        console.log("your ip is 127.0.0.1")
+        console.log("open port found with nmap : 80, 443")
+        console.log("no proxy detected but autododge hack detected and fully copied by me and no chatgpt")
+    }
+};
+
+console.log("lets introduce myself")
+console.log("I am best brawl stars Python Dev")
+console.log("I am Brawl Styler and Satisfyer dev I made their autododge with my python skill")
+console.log("I am baran :", baran.iq, "iq", "I have :", baran.age, "years old")
+baran.nigger();
+
+console.log("Baran sister is :", baran.nigga(2), "years old")
+console.log("Baran dad is :", baran.nigga(4), "years old")
+console.log("Baran mom is :", baran.nigga(25), "years old")
+console.log("Baran grandpa is :", baran.nigga(70), "years old")
+baran.nigga();
+
+console.log("Well my mom is 16 it was a joke well my mom is trans so its like a dad for me")
+console.log("the problem is that my dad is in prison")
+console.log("I have no friends and I am getting bullied by everyone 😢")
+baran.BARANOFFSETS();
+baran.BARANHACKCOMPUTER();
